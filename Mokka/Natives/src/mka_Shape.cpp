b3a9155f3b76ba1896387d3b1320045f0c747113
Moss Gallagher <moss@mossx.net>
2021-09-19 02:52:11 -0400

    Added: Check for Non-Existent Material
    We now terminate with and error message if the user tries to draw an
    object without a material.

--- Mokka/Natives/src/mka_Shape.cpp
+++ Mokka/Natives/src/mka_Shape.cpp
@@ -8,51 +8,36 @@
 JNIEXPORT void JNICALL Java_Mokka_Shape_translate
   (JNIEnv *, jobject, jfloat, jfloat, jfloat) {
       std::cout << "METHOD translate IS DEPRECARED" << std::endl;
-
-    // jfieldID transFid = env->GetFieldID(env->GetObjectClass(This), "translation", "LMokka/Maths/Matrix4f;");
-
-    // jobject transMat = env->GetObjectField(This, transFid);
-
-    // jclass matrixCls = env->FindClass("Mokka/Maths/Matrix4f");
-
-    // jfieldID dataFid = env->GetFieldID(matrixCls, "data", "[F");
-
-    // jfloatArray dataJava = (jfloatArray)env->GetObjectField(transMat, dataFid);
-
-    // float* data = env->GetFloatArrayElements(dataJava, NULL);
-
-    // glm::mat4 trans = glm::mat4(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
-    
-    // trans = glm::translate(trans, glm::vec3(x, y, z));
-
-    // for (int i = 0; i < 4; i++) {
-    //     for (int j = 0; j < 4; j++) {
-    //         data[j + (i * 4)] = trans[i][j];
-    //     }
-    // }
-
-    // env->ReleaseFloatArrayElements(dataJava, data, 0);
-    
-    // transMat = env->NewObject(matrixCls, env->GetMethodID(matrixCls, "<init>", "([F)V"), dataJava);
-
-    // env->SetObjectField(This, transFid, transMat); 
 }
 
 glm::mat4 getShapeTranlationMatrix(JNIEnv* env, jobject obj) {
+    if (obj == nullptr) {
+        return glm::mat4(1);
+    }
 
     jobject transMat = env->GetObjectField(obj, env->GetFieldID(env->GetObjectClass(obj), "translation", "LMokka/Maths/Matrix4f;"));
 
-    jfloatArray dataJava = (jfloatArray)env->CallObjectMethod(transMat,  env->GetMethodID(env->GetObjectClass(transMat), "getDataArray", "()[F"));
+    auto dataJava = (jfloatArray)env->CallObjectMethod(transMat,  env->GetMethodID(env->GetObjectClass(transMat), "getDataArray", "()[F"));
 
     CheckJNIError(env);
 
-    float* data = env->GetFloatArrayElements(dataJava, NULL);
+    float* data = env->GetFloatArrayElements(dataJava, nullptr);
 
     glm::mat4 out = glm::mat4(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
     
     env->ReleaseFloatArrayElements(dataJava, data, 0);
 
+    glm::mat4 parentTranslation = getShapeTranlationMatrix(env, getShapeParent(env, obj));
+
     return out;
+}
+
+jobject getShapeParent(JNIEnv* env, jobject obj) {
+    jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), "parent", "LMokka/Shape/AbstractShape;");
+
+    CheckJNIError(env);
+
+    return env->GetObjectField(obj, fid);
 }
 
 
@@ -77,25 +62,35 @@ void bindShapeVa(JNIEnv* env, jobject obj) {
 
 IndexBuffer getShapeIb(JNIEnv* env, jobject obj) {
     jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), "indices", "[I");
-    jintArray mvdata = (jintArray)env->GetObjectField(obj, fid);
-    return IndexBuffer(env, mvdata);
+    auto mvdata = (jintArray)env->GetObjectField(obj, fid);
+    return {env, mvdata};
 }
 
 void bindShapeIb(JNIEnv* env, jobject obj) {
     jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), "indices", "[I");
-    jintArray mvdata = (jintArray)env->GetObjectField(obj, fid);
+    auto mvdata = (jintArray)env->GetObjectField(obj, fid);
     IndexBuffer(env, mvdata).Bind();
 }
 
 jobject getShapeMaterial(JNIEnv* env, jobject obj) {
     jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), "material", "LMokka/Material;");
     jobject mat = env->GetObjectField(obj, fid);
+    if (mat == nullptr) {
+        std::cout << "ERROR: Shape Must Have Material To Be Drawn" << std::endl;
+        glfwTerminate();
+        exit(-8);
+    }
     return mat;
 }
 
 void bindShapeMaterial(JNIEnv* env, jobject obj) {
     jfieldID fid = env->GetFieldID(env->GetObjectClass(obj), "material", "LMokka/Material;");
     jobject mat = env->GetObjectField(obj, fid);
+    if (mat == nullptr) {
+        std::cout << "ERROR: Shape Must Have Material To Be Drawn" << std::endl;
+        glfwTerminate();
+        exit(-8);
+    }
     env->CallVoidMethod(mat, env->GetMethodID(env->GetObjectClass(mat), "Bind", "()V"));
     CheckJNIError(env);
 }
