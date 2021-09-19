86f2634b0831c8dc2547a8c135f87fdf63e5954a
Moss Gallagher <moss@mossx.net>
2021-09-19 12:20:45 -0400

    Added: Testing Children

--- Examples/BasicRedSquare.java
+++ Examples/BasicRedSquare.java
@@ -3,24 +3,44 @@ package Examples;
 import Mokka.*;
 import Mokka.Shape.*;
 
-public class BasicExample {
+public class BasicRedSquare {
     static Rect square;
-    static float[] c = {0.3f, 0.8f, 1f, 1};
+    static Rect child1;
+    static Rect child2;
 
 
 
     public static void init() {
-        Material red = new Material.BasicColor(new Color(1, 0, 0, 1));
+        Material red = new Material.BasicTexture(new Texture("Mokka/res/Textures/java.jpeg"));
 
         square = new Rect(0, 0, 0.5f,0.5f);
 
         square.setMaterial(red);
+
+        child1 = new Rect(.9f, 0, .2f, .2f);
+
+        child1.setMaterial(red);
+
+        child2 = new Rect(0, 0.5f, .25f, .25f);
+
+        child2.setMaterial(red);
+
+        square.addChild(child1);
+
+        square.addChild(child2);
+
+        square.setController(new PlayerController2D(.005f));
+
+        child2.setController(new PlayerController2D(.01f));
     }
 
     public static void run() {
 
         square.draw();
 
+        child1.draw();
+
+        child2.draw();
     }
 
 
